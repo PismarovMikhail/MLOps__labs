2f36907ef1c9142b24ec0d9ae284a8b61313f200
PismarovMikhail <lsantifico@gmail.com>
2023-06-12 21:40:38 +0000

    Make first version of code

--- lab1/data_preprocessing.py
+++ lab1/data_preprocessing.py
@@ -1,1 +1,22 @@
-# Script for data preprocessing
+# Script for dataset normalization 
+import pandas as pd
+import sklearn
+from sklearn.preprocessing import MinMaxScaler
+from data_creation import load_dataset 
+# Открытие файла с загруженныи датасетом
+
+#file = open('/home/prof/MLOps/MLOps_labs/MLOps_first_semester_labs/lab1/data_creation.py', 'r')
+
+powerlifting_norm = load_dataset
+
+data = powerlifting_norm
+names = data.columns
+scaler = MinMaxScaler()
+data = scaler.fit_transform(data)
+
+powerlifting_norm = pd.DataFrame(data, columns=names)
+
+print('\nDataset with normalization\n')
+print(powerlifting_norm)
+
+
