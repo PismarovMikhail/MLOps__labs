b2ac08c25fde40a6092fb8689d414e214be915f7
PismarovMikhail <lsantifico@gmail.com>
2023-06-12 15:45:16 +0000

    Add dataset name

--- lab1/data_creation.py
+++ lab1/data_creation.py
@@ -1,5 +1,6 @@
  # Script to load dataset
 import pandas as pd
 load_dataset = pd.read_csv('/home/prof/google-disk/kaggle_datasets/powerlifting/numeric_powerlifting_man.csv')
+print('\nLoaded Dataset\n')
 print(load_dataset)
 
