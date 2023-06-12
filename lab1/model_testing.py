abcdbe599ffd6c31911e5e4e47617dc899b98a81
PismarovMikhail <lsantifico@gmail.com>
2023-06-12 23:41:42 +0000

    Create code

--- lab1/model_testing.py
+++ lab1/model_testing.py
@@ -1,17 +1,20 @@
-# model validation script 
+#  model validation script 
 import numpy as np
 import pandas as pd
 import sklearn
-from sklearn.neighbors import KNeighborsRegressors
+from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import cross_validate
 from sklearn.model_selection import ShuffleSplit
-from sklearn.model_metrics import mean_squared_error as mse
-from sklearn.model_selection import r2_score
+from sklearn.metrics import mean_squared_error as mse
+from sklearn.metrics import r2_score
+from data_preprocessing import powerlifting_norm
 
 X_test = np.load(open('X_test', 'rb'))
 y_test = np.load(open('y_test', 'rb'))
 X_train = np.load(open('X_train', 'rb'))
 y_train = np.load(open('y_train', 'rb'))
+
+model = powerlifting_norm
 
 y_predict = model.predict(X_test)
 
@@ -23,7 +26,7 @@ scoring = {'R2': 'r2',
 
 scores = cross_validate(model, X_train, y_train, scoring = scoring,
                        cv= ShuffleSplit(n_splits=5, random_state=42)
-                       )) 
+                       ) 
 DF_cv_kNN = pd.DataFrame(scores)
 
 print('\nОшибка на тестовых данных\n')
