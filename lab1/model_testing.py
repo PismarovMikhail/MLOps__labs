b463943584711f6574165d8466e9d116ba6464a5
PismarovMikhail <lsantifico@gmail.com>
2023-06-13 00:05:21 +0000

    Create code

--- lab1/model_testing.py
+++ lab1/model_testing.py
@@ -3,20 +3,31 @@ import numpy as np
 import pandas as pd
 import sklearn
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_validate
 from sklearn.model_selection import ShuffleSplit
 from sklearn.metrics import mean_squared_error as mse
 from sklearn.metrics import r2_score
 from data_preprocessing import powerlifting_norm
 
-X_test = np.load(open('X_test', 'rb'))
-y_test = np.load(open('y_test', 'rb'))
-X_train = np.load(open('X_train', 'rb'))
-y_train = np.load(open('y_train', 'rb'))
 
-model = powerlifting_norm
+X, y = powerlifting_norm.drop(columns = ['Wilks']).values, powerlifting_norm['Wilks'].values
 
-y_predict = model.predict(X_test)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42) 
+
+
+# создаем объект класса с указанием гиперпараметров
+k = 8
+weights = 'distance'
+
+kNN = KNeighborsRegressor(n_neighbors=k, 
+                          weights=weights )
+
+# обучаем на тренировочных данных
+kNN.fit(X_train, y_train)
+
+# предсказываем на тестовых данных
+y_predict=kNN.predict(X_test)
 
 scoring = {'R2': 'r2',
            '-MSE': 'neg_mean_squared_error',
@@ -24,7 +35,7 @@ scoring = {'R2': 'r2',
            '-Max': 'max_error'
            }
 
-scores = cross_validate(model, X_train, y_train, scoring = scoring,
+scores = cross_validate(kNN, X_train, y_train, scoring = scoring,
                        cv= ShuffleSplit(n_splits=5, random_state=42)
                        ) 
 DF_cv_kNN = pd.DataFrame(scores)
