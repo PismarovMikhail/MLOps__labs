0015fd150789faf59285afbc9a09b474f741b86e
PismarovMikhail <mikepiamarov@gmail.com>
2023-06-24 04:30:16 +0300

    Modify code

--- lab1/data_creation.py
+++ lab1/data_creation.py
@@ -1,8 +1,7 @@
  # Script to load dataset
 import pandas as pd
+
 load_dataset = pd.read_csv('/home/mike/magistracy/google_drive/kaggle_datasets/powerlifting/powerlifting_man_old/numeric_powerlifting_man.csv')
+
 print('\nLoaded Dataset\n')
 print(load_dataset)
-"""
-/home/mike/magistracy/google_drive/kaggle_datasets/powerlifting/powerlifting_man_old
-"""
