01383558a41bee51fdfe812325e9e6115ba90316
PismarovMikhail <mikepiamarov@gmail.com>
2023-06-24 04:26:47 +0300

    Create new location

--- lab1/data_creation.py
+++ lab1/data_creation.py
@@ -1,6 +1,8 @@
  # Script to load dataset
 import pandas as pd
-load_dataset = pd.read_csv('/home/prof/google-disk/kaggle_datasets/powerlifting/numeric_powerlifting_man.csv')
+load_dataset = pd.read_csv('/home/mike/magistracy/google_drive/kaggle_datasets/powerlifting/powerlifting_man_old/numeric_powerlifting_man.csv')
 print('\nLoaded Dataset\n')
 print(load_dataset)
-
+"""
+/home/mike/magistracy/google_drive/kaggle_datasets/powerlifting/powerlifting_man_old
+"""
