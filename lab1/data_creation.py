a4b31de3a986496a53d70261634c302ac42a375d
PismarovMikhail <mikepiamarov@gmail.com>
2023-06-24 07:20:24 +0300

    Create data_creation.py file

--- lab1/data_creation.py
+++ lab1/data_creation.py
@@ -4,4 +4,4 @@ import pandas as pd
 load_dataset = pd.read_csv('/home/mike/magistracy/google_drive/kaggle_datasets/powerlifting/powerlifting_man_old/numeric_powerlifting_man.csv')
 
 print('\nLoaded Dataset\n')
-print(load_dataset)
+print(load_dataset) 
