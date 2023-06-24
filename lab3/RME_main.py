74744faef7fb1aca49c9657f3a6b4e09f2232e36
PismarovMikhail <mikepiamarov@gmail.com>
2023-06-24 07:55:30 +0300

    Create RME_main.py file

--- lab3/RME_main.py
+++ lab3/RME_main.py
@@ -1,4 +1,5 @@
- # Script to load dataset, for dataset normalization, for train_test model
+# Script for dataset load, for dataset normalization, for train_test model
+# for y prediction 
 import pandas as pd
 import numpy as np
 import sklearn
@@ -7,17 +8,15 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_validate
 from sklearn.model_selection import ShuffleSplit
-from sklearn.metrics import mean_squared_error as mse
 from sklearn.metrics import r2_score
 
 
-# Script to load dataset,
-load_dataset = pd.read_csv('/home/prof/google-disk/kaggle_datasets/powerlifting/numeric_powerlifting_man.csv')
+# Load dataset
+load_dataset = pd.read_csv('/home/mike/magistracy/google_drive/kaggle_datasets/powerlifting/powerlifting_man_old/numeric_powerlifting_man.csv')
 print('\nLoaded Dataset\n')
 print(load_dataset)
 
-
-# Script for dataset normalization
+# Dataset normalization
 powerlifting_norm = load_dataset
 
 data = powerlifting_norm
@@ -30,31 +29,25 @@ powerlifting_norm = pd.DataFrame(data, columns=names)
 print('\nDataset with normalization\n')
 print(powerlifting_norm)
 
-
-# Script for train_test model
+# Split dataset
 
 X, y = powerlifting_norm.drop(columns = ['Wilks']).values, powerlifting_norm['Wilks'].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-
-# создаем объект класса с указанием гиперпараметров
+# Create model
 k = 8
 weights = 'distance'
 
 kNN = KNeighborsRegressor(n_neighbors=k,
                           weights=weights )
 
-# обучаем на тренировочных данных
+# train on train data
 kNN.fit(X_train, y_train)
 
-# предсказываем на тестовых данных
+# predict on test data
 y_predict=kNN.predict(X_test)
 
-scoring = {'R2': 'r2',
-           '-MSE': 'neg_mean_squared_error',
-           '-MAE': 'neg_mean_absolute_error',
-           '-Max': 'max_error'
-           }
+scoring = {'R2': 'r2'}
 
 scores = cross_validate(kNN, X_train, y_train, scoring = scoring,
                        cv= ShuffleSplit(n_splits=5, random_state=42)
@@ -62,6 +55,4 @@ scores = cross_validate(kNN, X_train, y_train, scoring = scoring,
 DF_cv_kNN = pd.DataFrame(scores)
 
 print('\nОшибка на тестовых данных\n')
-
-print('MSE: %.5f' % mse(y_test, y_predict))
-print('RME: %.5f' % mse(y_test, y_predict, squared=False))
+print('R2: %.5f' % r2_score(y_test, y_predict))
